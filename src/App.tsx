a204b5c6e83ca8ab57b8213e5c3c635aace0ebc7
pdavinunes <p.davi.nunes@gmail.com>
2022-10-31 09:04:59 -0300

    feat: add rotas e default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from "react-router-dom";
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter >
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  max-width: 70rem;
+  margin: 2rem auto;
+  
+  display: flex;
+  flex-direction: column;
+`

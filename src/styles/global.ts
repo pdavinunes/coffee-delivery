d62ac450ffa9919ee521a2f18d194a2f091e4c7f
pdavinunes <p.davi.nunes@gmail.com>
2023-01-06 08:33:32 -0300

    chore: global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,12 +6,8 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-  :focus {
-    outline: none;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['yellow-dark']};
-  }
   body {
-    background: ${(props) => props.theme['background']};
+    background: ${(props) => props.theme.background};
     color: ${(props) => props.theme['base-text']};
     -webkit-font-smoothing: antialiased;
   }

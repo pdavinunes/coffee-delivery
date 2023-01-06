35e23e773ac8f395307ea0a55f600bdfe1817cfe
pdavinunes <p.davi.nunes@gmail.com>
2023-01-06 08:33:21 -0300

    chore: update layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,9 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 70rem;
-  margin: 2rem auto;
-  
   display: flex;
   flex-direction: column;
 `
